730e43615b80b236c1674b9650c29bba5001a77c
ugo <ugo@163.com>
2021-07-19 11:30:26 +0800

    SprigBoot Initial commit

---
+++ src/main/java/com/ugo/SpringBootDemo.java
@@ -0,0 +1,12 @@
+package com.ugo;
+
+/**
+ * @author: admin
+ * @createTime: 2021年07月19日
+ * @Description：
+ */
+public class SpringBootDemo {
+    public static void main(String[] args) {
+        System.out.println("springboot edit one,,,");
+    }
+}
